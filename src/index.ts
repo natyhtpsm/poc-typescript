ff2db580e9a526e4223d9c1fa64a48272f765146
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-09-15 16:59:12 -0300

    Feat: app ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 //delete, update, post, get
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+});
