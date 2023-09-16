65f65ac6be2fb1838873deefbe8eaa112e3ef6b2
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-09-15 21:50:52 -0300

    Update: schema

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -4,7 +4,7 @@ export const filmeSchema = Joi.object({
     nome: Joi.string().required(),
     plataforma: Joi.string().required(),
     genero: Joi.string().required(),
-    status: Joi.string().required(),
+    status: Joi.string().valid('Quero assistir', 'Em andamento', 'Finalizado').required(),
     nota: Joi.number().optional(),
     resumo: Joi.string().optional(),
 });
